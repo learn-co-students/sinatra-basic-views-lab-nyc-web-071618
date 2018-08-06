1848e36688033e35098308c64430c33a8adbe639
yunglleung1 <yungl.leung1@gmail.com>
2018-08-06 10:35:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    status 200
+    erb :index
+  end
 
 end
+
+# returns a 200 status code
+# renders index.erb
+#
+# displays the name of the bowling alley in an h1 tag
+# displays the bowling alley tagline in an h2
+# displays the bowling alley address in a p tag
